494a6167169b2ca837cc86c4f9212d31ae442cd7
Abbas Ogaji <abbasogaji@gmail.com>
2019-05-12 18:39:42 +0100

    Added Owners List and Displayed at Owner's Index page. Closes #16

--- pet-clinic-web/src/main/java/abbas/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/abbas/springframework/springpetclinic/controllers/OwnerController.java
@@ -1,6 +1,7 @@
 package abbas.springframework.springpetclinic.controllers;
 
 
+import abbas.springframework.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
 
     @RequestMapping("")
     public String getOwners(Model model){
-        model.addAttribute("ownerName", "Abbas Ogaji");
+        model.addAttribute("owners",  this.ownerService.findAll());
         return "owner/index";
     }
 }
